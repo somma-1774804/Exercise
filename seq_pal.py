fc5df6c0e04d3ba9719f68e50099fd72f1dfc750
Francesco Somma <41836818+somma-1774804@users.noreply.github.com>
2020-07-08 10:44:34 +0200

    Update seq_pal.py

--- seq_pal.py
+++ seq_pal.py
@@ -1,7 +1,14 @@
+# Dato un intero n, stampa tutte le stringhe palindromi lunghe 2n con valori
+# {a,b}. L'algoritmo deve avere complessita' O(n2^n)
+
 def seq_pal_c(n):
     return seq_pal(2,[],0,2*(n)-1)
+
+# Usando i due indici i e j , rispettivamente inizializzati a 0 e 2n-1, incremento i e decremento j fino a ad arrivare ad n.
+# A questo punto j mi dirà cosa posso o non posso mettere nella seconda metà della sequenza scorrendo la metà sequenza da n a 0.
+
 def seq_pal(n,sol,i,j):
-    if i == (2*n) :
+    if i == (2*n) :             
         print(''.join(sol))
     else :
         if i<=n-1 or sol[j]=='a':
