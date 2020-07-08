122f1c5117f7465cb1ca6ec8b85ff8e25f6b51a4
Francesco Somma <41836818+somma-1774804@users.noreply.github.com>
2020-07-08 17:05:53 +0200

    Update matr_cresc.py

--- matr_cresc.py
+++ matr_cresc.py
@@ -1,33 +1,34 @@
 def formatta_matrice(matrice):
     return '\n'.join([''.join(['{:2}'.format(x) for x in i]) for i in matrice])
 
-def matr_crescente(n):
-    def matr_crescente_bt(n, i, j, sol):
+
+def matr_crescente_bt(n, i, j, sol, c):
         set = {1, 2, 3}
-        def matr_cresc2(n, i, j, sol,c):
-           sol[i][j]=c
-           if j < n - 1:
-               matr_crescente_bt(n, i, j + 1, sol)
-           else:
-               matr_crescente_bt(n, i + 1, 0, sol)
 
         if i==n:
            print(formatta_matrice(sol))
            print()
-        else :
-            for y in set:
-                if i==0 and j==0: matr_cresc2(n,i,j,sol,y)
-                vicini_ok=True
-                if i==0 and j>0:
-                    vicini_ok &= sol[i][j-1]<=y
-                if i>0 and j>0:
-                    vicini_ok &= sol[i-1][j]<=y and sol[i][j-1]<=y
-                if i>0 and j==0:
-                    vicini_ok &= sol[i-1][j]<=y and sol[i][j+1]>=y
 
-            if vicini_ok : matr_cresc2(n,i,j,sol,y)
+        elif i>0 :
+            sol[i][j] = c
+            if j < n - 1:
+                matr_crescente_bt(n, i, j + 1, sol)
+            else:
+                matr_crescente_bt(n, i + 1, 0, sol)
 
+        for y in set:
+            if i==0 and j==0: matr_crescente_bt(n,i,j,sol,y)
 
+            vicini_ok = True
+            if i==0 and j>0:
+                 vicini_ok &= sol[i][j-1]<=y
+            if i>0 and j>0:
+                 vicini_ok &= sol[i-1][j]<=y and sol[i][j-1]<=y
+            if i>0 and j==0:
+                 vicini_ok &= sol[i-1][j]<=y and sol[i][j+1]>=y
 
-    sol = [[0 for j in range(n)] for i in range(n)]
-    matr_crescente_bt(2,0,0,sol)
+            if vicini_ok : matr_crescente_bt(n,i,j,sol,y)
+
+sol = [[0 for _ in range(n)] for _ in range(n)]
+matr_crescente_bt(2,0,0,sol,0)
+
