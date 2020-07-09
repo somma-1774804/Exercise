3af73790ce332e39d90d8a25fb71991395b499a4
Francesco Somma <41836818+somma-1774804@users.noreply.github.com>
2020-07-09 10:09:47 +0200

    Add files via upload

--- matr_cresc.py
+++ matr_cresc.py
@@ -1,30 +1,36 @@
 def formatta_matrice(matrice):
     return '\n'.join([''.join(['{:2}'.format(x) for x in i]) for i in matrice])
 
+def matr_crescente_bt(len, i, j, sol):
+    set = {1, 2, 3}
+    def matr_cresc_bk(len,i,j,sol,c):
+            sol[i][j] = c
+            if j < len - 1:
+                matr_crescente_bt(len, i, j + 1, sol)
+            else:
+                matr_crescente_bt(len, i + 1, 0, sol)
 
-def matr_crescente_bt(n, i, j, sol):
-        set = {1, 2, 3}
+    if i==len:
+       print(formatta_matrice(sol))
+       print()
+    else :
+          for y in set:
+                 if i==0 and j==0:
+                    matr_cresc_bk(len,i,j,sol,y)
+                            
+                 vicini_ok = True
+                 if i==0 and j>0:
+                    vicini_ok &= sol[i][j-1]<=y
+                 if i>0 and j>0:
+                    vicini_ok &= sol[i-1][j]<=y and sol[i][j-1]<=y
+                 if i>0 and j==0:
+                    vicini_ok &= sol[i-1][j]<=y and sol[i][j+1] <= y
 
-        if i==n:
-           print(formatta_matrice(sol))
-           print()
+                 if vicini_ok :
+                    matr_cresc_bk(len,i,j,sol,y)
 
-        for y in set:
-            vicini_ok = True
-            if i==0 and j>0:
-                 vicini_ok &= sol[i][j-1]<=y
-            if i>0 and j>0:
-                 vicini_ok &= sol[i-1][j]<=y and sol[i][j-1]<=y
-            if i>0 and j==0:
-                 vicini_ok &= sol[i-1][j]<=y and sol[i][j+1]>=y
 
-            if vicini_ok :
 
-                sol[i][j] = y
-                if j < n - 1:
-                    matr_crescente_bt(n, i, j + 1, sol)
-                else:
-                    matr_crescente_bt(n, i + 1, 0, sol)
-
-sol = [[0 for _ in range(n)] for _ in range(n)]
-matr_crescente_bt(2,0,0,sol,0)
+n=2
+sol = [[0 for j in range(n)] for i in range(n)]
+matr_crescente_bt(n,0,0,sol)
